7e580027981af6670e96b8b4f115c6ad92d5c613
Hydrophonix <hydrophonixfx@gmail.com>
2018-07-27 03:48:51 +0300

    added layout

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: 'airbnb',
+    parser: 'babel-eslint',
+    rules: {
+      'react/jsx-filename-extension': 0,
+      'react/jsx-one-expression-per-line': 0,
+      'react/prop-types': 0,
+    },
+};
